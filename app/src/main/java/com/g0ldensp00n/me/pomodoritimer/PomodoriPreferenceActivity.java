77492c91740c8ab587bed1e0a1915e9b00f6d6b0
Spoon <hayfithyan@gmail.com>
2017-07-28 20:38:59 -0700

    Lots of updates!

---
+++ app/src/main/java/com/g0ldensp00n/me/pomodoritimer/PomodoriPreferenceActivity.java
@@ -0,0 +1,80 @@
+package com.g0ldensp00n.me.pomodoritimer;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.inputmethodservice.KeyboardView;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class PomodoriPreferenceActivity extends PreferenceActivity {
+
+    private Toolbar bar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        SharedPreferences preferenceManager = PreferenceManager.getDefaultSharedPreferences(this);
+        preferenceManager.registerOnSharedPreferenceChangeListener(spChanged);
+        setSelectedTheme();
+    }
+
+    private void setSelectedTheme() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        switch(sharedPreferences.getString(getString(R.string.themePreferences), "Default")) {
+            case "Dark":
+                //Set Settings Dark Theme
+                setCurrentTheme(R.color.colorDark, R.color.colorDarkLight, R.color.colorDarkRing, R.color.textColorPrimary);
+                break;
+            default:
+                //Set Settings Light Theme
+                setCurrentTheme(R.color.settingLightThemeMain, R.color.settingLightThemeDark, R.color.settingLightThemeBackground, R.color.settingLightThemeFont);
+        }
+    }
+
+    SharedPreferences.OnSharedPreferenceChangeListener spChanged = new
+            SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    setSelectedTheme();
+                }
+            };
+
+    private void setCurrentTheme(int colorPrimary, int colorDark, int colorBackground, int textColor){
+        bar.getNavigationIcon().setTint(getColorInt(R.color.textColorPrimary));
+        bar.setBackgroundColor(getColorInt(colorPrimary));
+        bar.setTitleTextColor(getColorInt(R.color.textColorPrimary));
+        getWindow().setStatusBarColor(getColorInt(colorDark));
+        getWindow().setNavigationBarColor(getColorInt(colorPrimary));
+        getWindow().setBackgroundDrawable(new ColorDrawable(getColorInt(colorBackground)));
+        getView
+    }
+
+    private int getColorInt(int colorIn){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return getColor(colorIn);
+        } else {
+            Resources resources = getResources();
+            return resources.getColor(colorIn);
+        }
+    }
+}
--- app/src/main/java/com/g0ldensp00n/me/pomodoritimer/PomodoriTimer.java
+++ app/src/main/java/com/g0ldensp00n/me/pomodoritimer/PomodoriTimer.java
@@ -1,14 +1,29 @@
 package com.g0ldensp00n.me.pomodoritimer;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
@@ -16,7 +31,7 @@ public class PomodoriTimer extends AppCompatActivity {
 
     private Handler handler = new Handler();
     private TextView timerOutput;
-    private boolean runningTimer = false;
+    private boolean runningTimer = true;
     private boolean breakTime = false;
 
     @Override
@@ -28,7 +43,45 @@ public class PomodoriTimer extends AppCompatActivity {
         Typeface sfFont = Typeface.createFromAsset(getAssets(), "fonts/sf_font.ttf");
         timerOutput = (TextView) findViewById(R.id.timerText);
         timerOutput.setTypeface(sfFont);
+        setSelectedTheme();
         startRepeatingTask();
+
+        SharedPreferences settingManager = PreferenceManager.getDefaultSharedPreferences(this);
+        settingManager.registerOnSharedPreferenceChangeListener(spChanged);
+    }
+
+    private void setSelectedTheme() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        switch(sharedPreferences.getString(getString(R.string.themePreferences), "Default")) {
+            case "Dark":
+                setCurrentTheme(R.color.colorDark, R.color.colorDarkLight, R.color.colorDarkRing);
+                break;
+            default:
+                setCurrentTheme(R.color.colorPrimary, R.color.colorPrimaryDark, R.color.colorRing);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settingsMenuItem:
+                openSettingsView();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void openSettingsView() {
+        Intent settingsIntent = new Intent(this, PomodoriPreferenceActivity.class);
+        startActivity(settingsIntent);
     }
 
     @Override
@@ -58,6 +111,8 @@ public class PomodoriTimer extends AppCompatActivity {
                             String output = breakTime ? "25:00" : "5:00";
                             timerOutput.setText(output);
                             breakTime = !breakTime;
+                            if(breakTime) setCurrentTheme(R.color.colorBreak, R.color.colorBreakDark, R.color.colorBreakRing);
+                            else setSelectedTheme();
                         } else if (Integer.parseInt(timerStrings[1]) == 0) {
                             String output = Integer.parseInt(timerStrings[0]) - 1 + ":59";
                             timerOutput.setText(output);
@@ -72,6 +127,19 @@ public class PomodoriTimer extends AppCompatActivity {
         }
     };
 
+    private void setCurrentTheme(int colorPrimary, int colorDark, int colorRing){
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getColorInt(colorPrimary)));
+        getWindow().setStatusBarColor(getColorInt(colorDark));
+        getWindow().setNavigationBarColor(getColorInt(colorPrimary));
+        findViewById(R.id.mainBackground).setBackgroundColor(getColorInt(colorPrimary));
+        Drawable stop = getDrawable(R.drawable.ic_stop_primarycolor);
+        stop.setTint(getColorInt(colorPrimary));
+        ((FloatingActionButton) findViewById(R.id.floatingActionButton)).setImageDrawable(stop);
+        Drawable clockRing = getDrawable(R.drawable.clockring);
+        clockRing.setTint(getColorInt(colorRing));
+        findViewById(R.id.progressBar).setBackground(clockRing);
+    }
+
     Runnable progressBarUpdate = new Runnable() {
         int currentRemove = 0;
         int timeLast;
@@ -85,7 +153,12 @@ public class PomodoriTimer extends AppCompatActivity {
                         currentRemove = timeLast != timeCurrent ? 0 : currentRemove;
                         double currentTimerTime = timeCurrent - (currentRemove * 0.01);
                         double currentMax = breakTime ? 300.0 : 1500;
-                        progressBar.setProgress((int) ((currentTimerTime / currentMax) * 10000.0), true);
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            progressBar.setProgress(Math.abs((breakTime ? -10000 : 0) + (int) ((currentTimerTime / currentMax) * 10000.0)), true);
+                        }else {
+                            progressBar.setProgress(Math.abs((breakTime ? -10000 : 0) + (int) ((currentTimerTime / currentMax) * 10000.0)));
+                        }
                         timeLast = timeCurrent;
                         currentRemove++;
                     }
@@ -94,6 +167,14 @@ public class PomodoriTimer extends AppCompatActivity {
                 }
         }
     };
+
+    SharedPreferences.OnSharedPreferenceChangeListener spChanged = new
+            SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    if(!breakTime) setSelectedTheme();
+                }
+            };
 
     public void pauseTimer(View view) {
         if(runningTimer){
@@ -107,10 +188,23 @@ public class PomodoriTimer extends AppCompatActivity {
         if(view instanceof FloatingActionButton){
             FloatingActionButton button = (FloatingActionButton) view;
             if(runningTimer){
-                button.setImageResource(R.drawable.ic_stop_primarycolor);
+                Drawable drawableToTint = getDrawable(R.drawable.ic_stop_primarycolor);
+                drawableToTint.setTint(((ColorDrawable)findViewById(R.id.mainBackground).getBackground()).getColor());
+                button.setImageDrawable(drawableToTint);
             } else {
-                button.setImageResource(R.drawable.ic_play_primarycolor);
+                Drawable drawableToTint = getDrawable(R.drawable.ic_play_primarycolor);
+                drawableToTint.setTint(((ColorDrawable)findViewById(R.id.mainBackground).getBackground()).getColor());
+                button.setImageDrawable(drawableToTint);
             }
+        }
+    }
+
+    private int getColorInt(int colorIn){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return getColor(colorIn);
+        } else {
+            Resources resources = getResources();
+            return resources.getColor(colorIn);
         }
     }
 }
